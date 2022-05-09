29505bab0c8c3166f5dc65e41b905299f8801350
dindimon <dino.custic@gmail.com>
2022-05-09 11:03:22 +0200

    lombok config

--- src/main/java/com/danielfrak/code/keycloak/events/dtos/HybrisUserResponse.java
+++ src/main/java/com/danielfrak/code/keycloak/events/dtos/HybrisUserResponse.java
@@ -1,7 +1,9 @@
 package com.danielfrak.code.keycloak.events.dtos;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class HybrisUserResponse {
 	private String customerId;
--- src/main/java/com/danielfrak/code/keycloak/events/rest/UserService.java
+++ src/main/java/com/danielfrak/code/keycloak/events/rest/UserService.java
@@ -12,7 +12,7 @@ public class UserService implements HybrisUserService{
 
 	private final RestUserClient client;
 	public UserService(final Client restEasyClient) {
-		String uri = "http://192.168.178.36:9001/ocmws/v2/keycloak-user-migration";
+		String uri = "http://192.168.178.24:9001/ocmws/v2/keycloak-user-migration";
 		this.client = buildClient(restEasyClient, uri);
 	}
 
@@ -24,13 +24,15 @@ public class UserService implements HybrisUserService{
 
 	@Override
 	public boolean addHybrisUser(final HybrisUser user) {
-		final Response response = client.addHybrisUser(user);
-		return response.getStatus() == 200;
+		try (Response response = client.addHybrisUser(user)) {
+			return response.getStatus() == 200;
+		}
 	}
 
 	@Override
 	public boolean updateHybrisUser(final HybrisUser user) {
-		final Response response = client.updateHybrisUser(user);
-		return response.getStatus() == 200;
+		try (Response response = client.updateHybrisUser(user)) {
+			return response.getStatus() == 200;
+		}
 	}
 }
